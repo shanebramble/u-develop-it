e2755b1263e2ed11fe9ecade5e71b4be3b6f45f2
Shane Bramble-Wade <sbramwade@msn.com>
2021-09-06 15:18:03 -0400

    added a connection to the mysql database

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -8,6 +9,19 @@ app.use(express.urlencoded({
     extended: false
 }));
 app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // Your MySQL password
+        password: '@Pizza231996',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
