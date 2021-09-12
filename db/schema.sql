08f2d8fa1bb94983426f73937735b1e8ffb8a575
Shane Bramble-Wade <sbramwade@msn.com>
2021-09-11 19:23:22 -0500

    added LEFT JOIN to SQL query

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,23 @@
-CREATE TABLE candidates (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  industry_connected BOOLEAN NOT NULL 
-);
+-- As noted previously,the order of table creation is vital due to the dependency of 
+-- the candidates table on the existence of a parties.id.In the same regard,the 
+-- candidates table must be dropped before the parties table due to the foreign key 
+-- constraint that requires the parties table to exist.
+
+DROP TABLE IF EXISTS candidates;
+DROP TABLE IF EXISTS parties;
 
 CREATE TABLE parties (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(50) NOT NULL,
   description TEXT
 );
+
+CREATE TABLE candidates (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  party_id INTEGER,
+  industry_connected BOOLEAN NOT NULL,
+  CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties (id) ON DELETE SET NULL
+);
+
--- server.js
+++ server.js
@@ -25,7 +25,11 @@ const db = mysql.createConnection({
 
 // Return a database table of all candidates
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+    const sql = `SELECT candidates.*, parties.name 
+             AS party_name 
+             FROM candidates 
+             LEFT JOIN parties 
+             ON candidates.party_id = parties.id`;
     db.query(sql, (err, rows) => {
         if (err) {
             res.status(500).json({
@@ -42,7 +46,12 @@ app.get('/api/candidates', (req, res) => {
 
 // Return a single candidate
 app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const sql = `SELECT candidates.*, parties.name 
+             AS party_name 
+             FROM candidates 
+             LEFT JOIN parties 
+             ON candidates.party_id = parties.id
+             WHERE candidates.id = ?`;
     const params = [req.params.id];
 
     db.query(sql, params, (err, row) => {
@@ -83,7 +92,9 @@ app.delete('/api/candidate/:id', (req, res) => {
 });
 
 // Create a candidate
-app.post('/api/candidate', ({body}, res) => {
+app.post('/api/candidate', ({
+    body
+}, res) => {
     const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
     if (errors) {
         res.status(400).json({
